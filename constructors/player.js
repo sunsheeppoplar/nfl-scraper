ed08a95452b089fb68c273a36dcde113c2725e4d
Abner Yang <abtyang@gmail.com>
2018-04-02 01:02:22 -0400

    added more fields to save in each player instance

--- constructors/player.js
+++ constructors/player.js
@@ -1,7 +1,11 @@
-function Player(id, age, name) {
+function Player(id, age, college, draftPick, draftRound, name, position) {
 	this.id = id;
 	this.age = age;
+	this.college = college;
+	this.draftPick = draftPick;
+	this.draftRound = draftRound;
 	this.name = name;
+	this.position = position;
 }
 
 module.exports = Player;
