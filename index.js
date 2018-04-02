159ac954340aa95e0f92c48b50dea4ce59eb4954
Abner Yang <abtyang@gmail.com>
2018-04-02 01:03:06 -0400

    hardcoded range of years to scrape from 2017 to 2007, added some extra fields of interest such as position, college, etc.

--- index.js
+++ index.js
@@ -3,34 +3,46 @@ const cheerio = require('cheerio');
 const fs = require('fs')
 const Player = require('./constructors/player');
 
-const options = {
-	uri: `https://www.pro-football-reference.com/years/2017/draft.htm`,
-	transform: function(body) {
-		return cheerio.load(body);
-	}
+var initialYear = 2017
+var finalYear = 2007
+
+for (var i = initialYear; i >= finalYear; i--) {
+	scrapeDraftClass(i)
 }
 
-var players = [];
+function scrapeDraftClass(draftYear) {
+	const options = {
+		uri: `https://www.pro-football-reference.com/years/${draftYear}/draft.htm`,
+		transform: function(body) {
+			return cheerio.load(body);
+		}
+	}
 
-rp(options)
-	.then(($) => {
-		$("tbody tr").not('.thead').each((i, el) => {
-			var age =  $(el).children("[data-stat='age']").text()
-			var name = $(el).children("[data-stat='player']").text()
-			var id = i
+	var players = [];
 
-			var player = new Player(id, age, name)
-			players.push(player)
+	rp(options)
+		.then(($) => {
+			$("tbody tr").not('.thead').each((i, el) => {
+				var id = i
+				var age = $(el).children("[data-stat='age']").text()
+				var college = $(el).children("[data-stat='college_id']").text()
+				var draftPick = $(el).children("[data-stat='draft_pick']").text()
+				var draftRound = $(el).children("[data-stat='draft_round']").text()
+				var name = $(el).children("[data-stat='player']").text()
+				var position = $(el).children("[data-stat='pos']").text()
+
+				var player = new Player(id, age, college, draftPick, draftRound, name, position)
+				players.push(player)
+			})
+
+			fs.writeFile(`${draftYear}_draft.json`, JSON.stringify(players), function(err) {
+				if (err) throw err;
+				console.log(`${draftYear} draft completed`);
+				}
+			);
+
 		})
-
-		fs.writeFile("input.json", JSON.stringify(players), function(err) {
-			if (err) throw err;
-			console.log('complete');
-			}
-		);
-
-	})
-	.catch((err) => {
-		console.log(err)
-	})
-
+		.catch((err) => {
+			console.log(err)
+		})
+}
